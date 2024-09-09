62c982baa889d314bd80a2fa857f6bd246ba6ef4
Vaibhav Kumar <vaibhavkumar@Vaibhavs-MacBook-Air.local>
2024-09-09 22:33:00 +0530

    last changes 9th sept

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,8 +1,42 @@
+import styled from "styled-components";
+
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+  color: blue;
+`;
+
+const Input = styled.input`
+  border: 1px solid white;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  background-color: var(--color-brand-500);
+`;
+
+const StyledApp = styled.main`
+  background-color: orange;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <GlobalStyles />
+
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("check in")}>Check in</Button>
+
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
   );
 }
 
